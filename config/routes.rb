9759da538cd60a271bba55112f3b952086abd841
Christopher Lee <hanernlee@gmail.com>
2016-05-10 17:42:17 +1000

    fixed conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
 
   get '/' => 'users#index'
-<<<<<<< HEAD
 
   get '/api/smalltalks' => 'api/smalltalks#index'
 
-=======
-  
->>>>>>> b2d526ce3d330031fc9bd79758f25cf99b937ff6
 end
