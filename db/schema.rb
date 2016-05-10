86ad5acf148364d3494ed2056f35f6de28fda9db
Pranati Dave <pranati.dave@gmail.com>
2016-05-10 10:30:10 +1000

    created user and favourites model

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509233451) do
+ActiveRecord::Schema.define(version: 20160510002402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "favourites", force: :cascade do |t|
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.string   "favourites"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
